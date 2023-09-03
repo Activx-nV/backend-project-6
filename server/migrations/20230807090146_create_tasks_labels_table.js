318cfafb9d927babaf68a148b4ef2eb579bcbc79
Lauri <laurikamm@gmail.com>
2023-09-03 13:16:25 +0300

    fix eslint error

--- server/migrations/20230807090146_create_tasks_labels_table.js
+++ server/migrations/20230807090146_create_tasks_labels_table.js
@@ -2,16 +2,8 @@
 
 export const up = (knex) =>
   knex.schema.createTable('tasks_labels', (table) => {
-    table
-      .integer('task_id')
-      .references('id')
-      .inTable('tasks')
-      .onDelete('CASCADE');
-    table
-      .integer('label_id')
-      .references('id')
-      .inTable('labels')
-      .onDelete('CASCADE');
+    table.integer('task_id').references('id').inTable('tasks').onDelete('CASCADE');
+    table.integer('label_id').references('id').inTable('labels').onDelete('CASCADE');
   });
 
 export const down = (knex) => knex.schema.dropTable('tasks_labels');
