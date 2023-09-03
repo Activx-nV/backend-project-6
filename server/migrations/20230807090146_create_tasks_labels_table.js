2e890373df01e8b2257a5d2f9e2d336967cfbe5e
Lauri <laurikamm@gmail.com>
2023-09-03 13:33:37 +0300

    fix create tasks labels eslint error

--- server/migrations/20230807090146_create_tasks_labels_table.js
+++ server/migrations/20230807090146_create_tasks_labels_table.js
@@ -1,9 +1,8 @@
 // @ts-check
 
-export const up = (knex) =>
-  knex.schema.createTable('tasks_labels', (table) => {
-    table.integer('task_id').references('id').inTable('tasks').onDelete('CASCADE');
-    table.integer('label_id').references('id').inTable('labels').onDelete('CASCADE');
-  });
+export const up = (knex) => knex.schema.createTable('tasks_labels', (table) => {
+  table.integer('task_id').references('id').inTable('tasks').onDelete('CASCADE');
+  table.integer('label_id').references('id').inTable('labels').onDelete('CASCADE');
+});
 
 export const down = (knex) => knex.schema.dropTable('tasks_labels');
